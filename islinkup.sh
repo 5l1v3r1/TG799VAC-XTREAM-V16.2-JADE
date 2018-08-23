8a8bc1bd7462c38cfab872a024101be2e246bf0e
wuseman <info@sendit.nu>
2018-08-23 13:57:41 +0200

    Create islinkup.sh

---
+++ islinkup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Check if Link is up using devstatus and Json
+
+. /usr/share/libubox/jshn.sh
+
+WANDEV="$(uci get network.wan.ifname)"
+
+json_load "$(devstatus $WANDEV)"
+
+json_get_var var1 speed
+json_get_var var2 link
+
+echo "Speed: $var1"
+echo "Link: $var2"
